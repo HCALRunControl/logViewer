a94060a09f04325c3030df38a7ae6e85d7b8535e
John Charles Hakala <john_hakala@brown.edu>
2017-03-03 22:12:48 +0100

    make more sparkly

--- logHtml.py
+++ logHtml.py
@@ -6,16 +6,15 @@ def getHeader():
 <html>
   <head>
      <link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700" rel="stylesheet" type="text/css">
-     <link rel="stylesheet" type="text/css" href="/shifterHomePage.css"> 
+     <link rel="stylesheet" type="text/css" href="/jhakala/webHandsaw.css"> 
      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
      <!-- <script src="/jhakala/contrast.js"></script> -->
   </head>
   <!-- <body onload="setContrast();">
          do this in the python -->
   <body>
+    <div id="top"><img src="/jhakala/webHandsaw.png" width="50px"><h1>webHandsaw</h1></div>
     <div id="wrapper">
-    <h3>webHandsaw</h3>
-    <br>
     <!-- end header -->
 """
   return header
