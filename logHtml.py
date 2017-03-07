7da07d4299022f9cfa525047eb05f9a74c22ceb6
John Charles Hakala <john_hakala@brown.edu>
2017-03-07 01:55:35 +0100

    forgot to unhardcode some stuff

--- logHtml.py
+++ logHtml.py
@@ -1,24 +1,24 @@
 # helper functions to spit out chunks of html for viewLogs.py
 # John Hakala 3/2/2017
 
-def getHeader():
+def getHeader(subDir):
   header = """
 <html>
   <head>
      <title>webHandsaw -- HCAL logs</title>
      <link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700" rel="stylesheet" type="text/css">
-     <link rel="stylesheet" type="text/css" href="/jhakala/webHandsaw.css"> 
-     <link rel="icon" type="image/png" href="/jhakala/webHandsaw_black.png">
+     <link rel="stylesheet" type="text/css" href="/%s/webHandsaw.css"> 
+     <link rel="icon" type="image/png" href="/%s/webHandsaw_black.png">
      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-     <!-- <script src="/jhakala/contrast.js"></script> -->
+     <!-- <script src="/%s/contrast.js"></script> -->
   </head>
   <!-- <body onload="setContrast();">
          do this in the python -->
   <body>
-    <div id="top"><img src="/jhakala/webHandsaw.png" width="50px"><h1>webHandsaw</h1></div>
+    <div id="top"><img src="/%s/webHandsaw.png" width="50px"><h1>webHandsaw</h1></div>
     <div id="wrapper">
     <!-- end header -->
-"""
+""" % (subDir, subDir, subDir, subDir)
   return header
 
 def getFooter():
